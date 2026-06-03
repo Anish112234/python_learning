487a179aa42645fd32f1b14f8a7ad94659cc7dd7
Anish12-3 <anishlamichhane282@gmail.com>
2026-06-03 18:15:30 +0545

    bug fixed

--- calculator.py
+++ calculator.py
@@ -1,47 +1,51 @@
 #find bug
 #choice statement
-#bug here
-def addication():
-    value1 = float(input("Enter integer"))
-    value2 = float(input("Enter integer"))
+#fixed bug already
+def addition():
+    value1 = int(input("Enter integer"))#use int(bug apper)
+    value2 = int(input("Enter integer"))
     return value1 + value2
 
 def subtraction():
-    value1 = float(input("Enter integer"))
-    value2 = float(input("Enter integer"))
+    value1 = int(input("Enter integer"))
+    value2 = int(input("Enter integer"))
     return value1-value2
      
 def multiplication():
-    value1 = float(input("Enter integer"))
-    value2 = float(input("Enter integer"))
+    value1 = int(input("Enter integer"))
+    value2 = int(input("Enter integer"))
     return value1*value2
 
 def division():
-    value1 = float(input("Enter integer"))
-    value2 = float(input("Enter integer"))
+    value1 = int(input("Enter integer"))
+    value2 = int(input("Enter integer"))
     if value2 == 0:
          return "Error:division by zero is not avaiable"
     return value1/value2
 
 def calculator():
-     print("welocome To calculator:")
-     print("1:addication")
+     print("welcome To calculator:")
+     print("1:addition")
      print("2:subtraction")
      print("3:Multiplication")
      print("4:Division")
-     print("Enter 'q' to Quute.")
+     print("Enter 'q' to Quit.")
 
      while True:
-        choice = input("\nChoose an operation(1/2/3/4)or 'q' to Quite").strip()
+        choice = input("\nChoose an operation(1/2/3/4)or 'q' to Quit").strip()
         if choice == 'q':
-               print("Existing The calculator.Goodbye!")
+               print("Exiting The calculator.Goodbye!")
                break
           
-     if choice == '1':
-              print(f"the result of addication is : {addication()}")
-     elif choice =='2':
+        if choice == '1':
+              print(f"the result of addition is : {addition()}")
+        elif choice =='2':
              print(f"the result of subtraction is :{subtraction()}")
-     elif choice == '3':
+        elif choice == '3':
              print(f"the result of multiplication is :{multiplication()}")
-     elif choice == '4':
+        elif choice == '4':
              print(f"the result of division is :{division()}")
+        else:
+             print("Invalid choice")     #I missed else (bug apper)
+
+calculator()
